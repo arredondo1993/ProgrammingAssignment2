0c7aefc8c4c493538ad58091480239a8684457bd
arredondo1993 <danielarredondo2@gmail.com>
2020-01-30 00:24:45 -0300

    programming assigment 2 week 3 lexical scoping solucionado

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## se crea cache de matriz 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y){
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) s <<- solve
+  getinv <- function() s
+  list(set=set, get=get, getinv=getinv, setinv=setinv)
 }
 
 
-## Write a short comment describing this function
+## recuperar matriz invertida realizada
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getinv()
+        if(!is.null(s)){
+          message("matriz invertida")
+          return(s)
+        }
+        matriz <- x$get()
+        s <- solve(matriz, ...)
+        x$setinv(s)
+        s
 }
